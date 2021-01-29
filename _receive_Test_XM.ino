002615e19d0b93ddc61d7b2e4f82ecb0c763052c
philippetavernier <philippe.tavernier@gmail.com>
2021-01-29 13:20:57 +0100

    Add files via upload

---
+++ _receive_Test_XM.ino
@@ -0,0 +1,66 @@
+// demo: CAN-BUS Shield, receive data
+#include <mcp_can.h>
+#include <SPI.h>
+
+unsigned char Flag_Recv = 0;
+unsigned char len = 0;
+unsigned char buf[8];
+char str[20];
+int CAN_Id;
+
+void setup()
+{
+  CAN.begin(CAN_500KBPS);                       // init can bus : baudrate = 500k
+  attachInterrupt(0, MCP2515_ISR, FALLING);     // start interrupt
+  Serial.begin(115200);
+  
+  // Test XM ////////////////////////////////////////////////////////////////////////
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void MCP2515_ISR()
+{
+    Flag_Recv = 1;
+}
+
+void loop()
+{
+    if(Flag_Recv)                           // check if get data
+    {
+      Flag_Recv = 0;                        // clear flag
+      
+      Serial.println("CAN_BUS GET DATA!");
+      
+      // Test XM ////////////////////////////////////////////////////////////////////////
+      CAN_Id = CAN.getCanId();
+      CAN.readMsgBuf(&len, buf);            // read data,  len: data length, buf: data buf
+      Serial.print("data len = ");
+      Serial.println(len);
+      
+      for(int i = 0; i<len; i++)            // print the data
+      {
+        Serial.print(buf[i]);Serial.print("\t");
+      }
+      Serial.println();
+      Serial.println();
+      
+      if (CAN_Id == 0)
+      {
+        Serial.println("CAN_Id = 0");
+        digitalWrite(3,0);
+        digitalWrite(4,1);
+      }
+        
+      if (CAN_Id == 1)
+      {
+        Serial.println("CAN_Id = 1");
+        digitalWrite(3,1);
+        digitalWrite(4,0);
+      }  
+    }    
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
---
+++ _send_Test_XM.ino
@@ -0,0 +1,29 @@
+// demo: CAN-BUS Shield, send data
+#include <mcp_can.h>
+#include <SPI.h>
+
+void setup()
+{
+  Serial.begin(115200);
+  // init can bus, baudrate: 500k
+  if(CAN.begin(CAN_500KBPS) ==CAN_OK) Serial.print("can init ok!!\r\n");
+  else Serial.print("Can init fail!!\r\n");
+}
+
+unsigned char Data_Id_0[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+unsigned char Data_Id_1[8] = {8, 9, 10, 11, 12, 13, 14, 15};
+
+void loop()
+{
+  // send data:  id = 0x00, standrad flame, data len = 8, stmp: data buf
+  CAN.sendMsgBuf(0x00, 0, 8, Data_Id_0);
+  delay(500);                       // send data per 500ms
+  
+  // send data:  id = 0x01, standrad flame, data len = 8, stmp: data buf
+  CAN.sendMsgBuf(0x01, 0, 8, Data_Id_1);  
+  delay(500);                       // send data per 500ms
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
